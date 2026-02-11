c475bf9184aa528dda8500dd6dd75a6f4ab77cd4
Bryan Brenes <chinotrb@gmail.com>
2026-02-11 11:14:47 -0600

    segunda mejora

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // GitHub Pages serves this app from /billix-hours/
 export default defineConfig({
   base: '/billix-hours/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
